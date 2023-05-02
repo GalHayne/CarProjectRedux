a32dd4ce7f2ef26ad65edd82ef0b40d0106a95f0
Gal <galhayne.gmail.com>
2023-05-02 23:45:04 +0300

    update the cost of cars when changing

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,6 +1,18 @@
+import { useSelector } from "react-redux";
+
 function CarValue() {
-    return <div>
-        CarValue
+    const totalCost = useSelector(({ cars: { data, searchTeam } }) => {
+        const filteredCars = data.filter((car) => {
+            return car.name.toLowerCase().includes(searchTeam.toLowerCase());
+        })
+        return filteredCars.reduce((acc, car) => {
+            return acc + car.cost;
+        }, 0)
+
+    });
+
+    return <div className="car-value">
+        Total Cost: ${totalCost}
     </div>
 }
 
