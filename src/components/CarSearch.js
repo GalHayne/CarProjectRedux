21657949416b890999c17dad20629fa1175feb27
Gal <galhayne.gmail.com>
2023-05-02 23:13:56 +0300

    change input onChange done

--- src/components/CarSearch.js
+++ src/components/CarSearch.js
@@ -1,5 +1,32 @@
+import { useDispatch, useSelector } from "react-redux";
+import { changeSearchTerm } from "../store/slices/carsSlice"
+
 function CarSearch() {
-    return <div>CarSearchBranch</div>
+    const dispath = useDispatch();
+
+    const searchTeam = useSelector((state) => {
+        console.log(state.cars.searchTeam);
+        return state.cars.searchTeam;
+
+    })
+
+    const handleSearchTermChange = (event) => {
+        dispath(changeSearchTerm(event.target.value));
+    };
+
+
+    return <div className="list-header">
+        <h3 className="title is-3">My Cars</h3>
+        <div className="search field is-horizontal">
+            <label className="label">Search</label>
+            <input
+                className="input"
+                value={searchTeam}
+                onChange={handleSearchTermChange}
+            />
+        </div>
+
+    </div>
 }
 
 export default CarSearch;
